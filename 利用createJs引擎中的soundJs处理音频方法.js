d734746ce4433858b34834f0d68e4fe99eb639af
hehe-PC\hehe <314084611@qq.com>
2017-03-31 11:40:22 +0800

    no message

--- 利用createJs引擎中的soundJs处理音频方法.js
+++ 利用createJs引擎中的soundJs处理音频方法.js
@@ -1,29 +1,30 @@
 // created by bai on 2017/02/20
 // 利用createJs引擎中的soundJs处理音频方法
 
-// 首先引用preloadjs/soundjs文件：
-// <script src="https://code.createjs.com/preloadjs-0.6.2.min.js"></script>
+// 首先引用soundjs文件：
 // <script src="https://code.createjs.com/soundjs-0.6.2.min.js"></script>
 
-var queue = new createjs.LoadQueue();
-queue.installPlugin(createjs.Sound);
-queue.addEventListener("complete", handleComplete, this);
-// queue.loadFile({id:"sound", src:"http://path/to/sound.mp3"});       //加载单个资源
-queue.loadManifest([                                                //加载资源列表清单
-    {id: "sound1", src:"sound1.mp3"}
-]);
+var sounds = {
+    path:"./music/",                       //音频所在文件夹path   
+    manifest: [
+        {id: "sound1", src:"sound1.mp3"}   //自定义音频id名，音频src
+    ]
+};
+createjs.Sound.alternateExtensions = ["mp3"];
+createjs.Sound.addEventListener("fileload", readytoplayAudio);
+createjs.Sound.registerSounds(sounds);
 
-function handleComplete() {
+function readytoplayAudio() {
     // 音频加载完成
     console.log("音频加载完成");
 }
 
-function playAudio(id) {
+function playAudio() {
     // 音频播放
-    createjs.Sound.play(id, {interrupt: createjs.Sound.INTERRUPT_ANY, delay:500, offset:0, loop:-1, colume:1, pan:-1, startTime:0, duration:500});          //利用id控制播放音频（可选播放属性：interrupt、delay、offset、loop、volume、pan、startTime、duration）
+    createjs.Sound.play("sound1");          //利用id控制播放音频（可选播放属性：interrupt、delay、offset、loop、volume、pan、startTime、duration）
 }
 
-function stopAudio(id) {
+function stopAudio() {
     // 音频暂停
-    createjs.Sound.stop(id);          //利用id控制暂停音频
+    createjs.Sound.stop("sound1");          //利用id控制暂停音频
 }
